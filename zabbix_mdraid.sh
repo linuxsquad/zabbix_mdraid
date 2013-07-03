d6ef007e21e9dd206fec0ddd7df717afaf57d95b
Oleg B <oleg@system76.brohome.lan>
2013-07-02 23:11:24 -0500

    Implementing discovery mechanism

--- zabbix_mdraid.sh
+++ zabbix_mdraid.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
 
+item=""
 #echo "OPTIND is now $OPTIND"
-while getopts ":te:s:d:" optname
+while getopts ":Dm:e:s:d:" optname
 do
     case "$optname" in
-	"t")
-            echo "MD test status"
-	    echo mdadm --test /dev/md0
-            ;;
 	"e")
 	    # Extract string values
-	    item=$(mdadm --detail /dev/md0 | grep "${OPTARG}" | awk -F":" '{print $2}')
+	    item=$(mdadm --detail ${MD_dev} | grep "${OPTARG}" | awk -F":" '{print $2}')
 	    echo ${item// /}
             ;;
 	"s")
             # echo "Size of the array"
-	    item=$(mdadm --detail /dev/md0 | grep "${OPTARG}" | awk -F":" '{print $2}')
+	    item=$(mdadm --detail ${MD_dev} | grep "${OPTARG}" | awk -F":" '{print $2}')
 	    echo ${item%%\ \(*}
             ;;
 	"d")
             # echo "Devices in the array"
-	    item=$(mdadm --detail /dev/md0 | tail -n+2 | grep "/dev/" | awk -v x=${OPTARG} '$4 == x {print $5,$6,$7}' )
+	    item=$(mdadm --detail ${MD_dev} | tail -n+2 | grep "/dev/" | awk -v x=${OPTARG} '$4 == x {print $5,$6,$7}' )
 	    echo ${item}
             ;;
+	"m")
+	    # echo "Setting MD RAID"
+	    MD_dev="${OPTARG}"
+	    ;;
+	"D")
+	    # echo "Discovery"
+	    echo -e "{\n\t\"data\":[\n"	    
+	    echo $(mdadm -Es) | while read line
+	    do
+		MDdev=`echo $line | awk '{print $2}'`
+		echo -e "\t{ \"{#MD_DEVICE}\":\"${MDdev}\" },"
+	    done
+	    echo -e "\n\t]\n}"	    
+	    ;;
 	"?")
             echo "Unknown option $OPTARG"
             ;;
@@ -37,4 +48,5 @@ do
     esac
 #    echo "OPTIND is now $OPTIND"
 done
+
 
